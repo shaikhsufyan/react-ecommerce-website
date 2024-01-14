00ec067f3319e859fbbac74b90fd725a6e087025
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-01-14 13:30:28 +0530

    first changes

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import heels from "./images/heels.png"
+
+const Navbar = () =>{
+    const [showItem, setshowItem] = useState(false)
+    const showNav = () =>{
+        setshowItem(true)
+    }
+    const hideNav = () =>{
+        setshowItem(false)
+    }
+    return(
+        <>
+
+        <nav className={showItem ? "show-navitems" : "hide-navitems"} >
+            <div className="logo">
+                <h1>TrendyShoe</h1>
+                <img src={heels}/>
+            </div>
+            <ul>
+            
+
+                <li><NavLink onClick={()=>setshowItem(false)} className="nav-items" to="/">Home</NavLink></li>
+                <li><NavLink onClick={()=>setshowItem(false)} className="nav-items" to="/about">About</NavLink></li>
+                <li><NavLink onClick={()=>setshowItem(false)} className="nav-items" to="/product">Products</NavLink></li>
+                <li><NavLink onClick={()=>setshowItem(false)} className="nav-items" to="contact">Contact</NavLink></li>
+                <li><NavLink onClick={()=>setshowItem(false)} className="nav-items" to="/addtocart"><i class="fa-solid fa-cart-shopping"></i></NavLink></li>
+            </ul>
+            <div className="icons">
+            <i onClick={showNav} id="bar-icon" class="fa-solid fa-bars"></i>
+            <i onClick={hideNav} id="cross-icon" class="fa-solid fa-xmark"></i>
+
+            </div>
+        </nav>
+        </>
+    )
+}
+export default Navbar;
---
+++ src/Components/ProductDetail.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react"
+import { NavLink, useParams } from "react-router-dom";
+import { API } from "./API";
+import PageNavigation from "./PageNavigation";
+import MyImage from "./MyImage";
+import AddToCart from "./AddToCart";
+import { useCartContext } from "./Context/CartContext";
+ 
+
+const ProductDetail = () =>{
+    const {addToCart} = useCartContext();
+
+    const {id} = useParams();
+    console.log(id);
+    const [singlePage, setSinglePage] = useState(API);
+ 
+    const singleValues = singlePage.map((curr)=>{
+          
+    })
+    // console.log(singleValues);
+ const singleData = singlePage.filter((currentPage)=>{
+    return currentPage.id == id;
+ });
+  const{image, title,price, brand, color, rating, category }=singleData[0];
+ 
+      
+     
+    
+       
+    return(
+        <>
+        <PageNavigation title={title}/>
+         <div className="singlePage">
+              
+            <MyImage imgs={image} />
+            
+              
+            <div className="page-data">
+                <p id="product-title">{title}</p>
+                <p>Rating : {rating}</p>
+                <p>MRP : {price}</p>
+                <p className="product-discription">A product description is a form of marketing copy used to <br/> describe and explain the benefits of your product. In other <br/> words, it provides all the information and details of your <br/> product on your ecommerce site product description is <br/> a form of marketing copy used to</p>
+                <div className="icons">
+                <i class="fa-solid fa-truck"></i>
+                <i class="fa-solid fa-repeat"></i>
+                <i class="fa-solid fa-check"></i>
+                <i class="fa-solid fa-mobile"></i>
+                </div>
+                <p>Brand : {brand}</p>
+                <p>Category : {category}</p>
+                <input className="number-input" type="number" placeholder="1" min="1" max="10"/>
+                <div className="product-button">
+                <NavLink to="/addtocart">
+                    <button onClick={() => addToCart( image, title, price ,id)}>ADD TO CART</button>
+                     
+                </NavLink>
+                    
+                    <button >BUY NOW</button>
+
+                </div>
+
+            </div>
+         </div>
+           
+          
+        </>
+    )
+}
+export default ProductDetail;
